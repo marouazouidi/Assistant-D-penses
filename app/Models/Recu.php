dc44507fe8510034495d86146e7ed407a32f6e64
marouazouidi <zouidimaroua@gmail.com>
2026-06-11 15:52:21 +0100

    add Models

--- app/Models/Recu.php
+++ app/Models/Recu.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recu extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'text_brut',
+        'status',
+        'json',
+        'user_id'
+    ];
+
+    public function User(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function Depenses(){
+        return $this->hasMany(Depense::class);
+    }
 }
