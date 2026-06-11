ac58f7267fd9492e9e79ebeb3186651d7a5c7577
marouazouidi <zouidimaroua@gmail.com>
2026-06-11 15:42:26 +0100

    add Migrations

--- database/migrations/2026_06_11_135552_create_depenses_table.php
+++ database/migrations/2026_06_11_135552_create_depenses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('depenses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->int('quantity', 10, 2);
+            $table->integer('quantity');
             $table->decimal('price', 10, 2);
             $table->string('categorie');
             $table->foreignId('recu_id')->constrained()->cascadeOnDelete();
